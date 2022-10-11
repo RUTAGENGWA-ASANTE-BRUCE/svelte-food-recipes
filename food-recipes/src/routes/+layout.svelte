c136c082613ddd5fe3508383ffae606f12a015fd
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-10-11 20:51:34 +0200

    fit: adding nav bar

---
+++ food-recipes/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Nav from '../components/nav.svelte'
+</script>
+<Nav />
+<slot></slot>
+
+<footer>
+    <h1>Welcome to svelte</h1>
+</footer>
---
+++ food-recipes/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import {onMount} from 'svelte';
+    let data;
+     onMount(()=>{
+        fetch("https://api/spoonicular.com/recipes/complexSearch?apiKey=bab")
+     })
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
