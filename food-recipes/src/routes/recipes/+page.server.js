d9f284a5d6b5d2641e21c2fd4c6824e07dbf6b3c
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-10-13 10:09:58 +0200

    fit: changing API_KEY config

--- food-recipes/src/routes/recipes/+page.server.js
+++ food-recipes/src/routes/recipes/+page.server.js
@@ -1,10 +1,11 @@
 export const load = async () => {
 	const environment_variables = import.meta.env;
-	const res = await fetch(
-		`https://api.spoonacular.com/recipes/complexSearch?number=50&apiKey=${environment_variables.VITE_SPOONACULAR_APIK_KEY}`
-	);
+	const url = `https://api.spoonacular.com/recipes/complexSearch?number=50&apiKey=${environment_variables.VITE_SPOONACULAR_APIK_KEY}`;
+	const res = await fetch(url);
+	console.log(url);
 	const { results } = await res.json();
 	return {
+		url,
 		recipes: results
 	};
 };
