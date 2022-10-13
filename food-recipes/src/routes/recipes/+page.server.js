5e0bf280994399c5e132621b81e3f98c3d30a71e
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-10-13 11:26:18 +0200

    fix: addiing api key

--- food-recipes/src/routes/recipes/+page.server.js
+++ food-recipes/src/routes/recipes/+page.server.js
@@ -1,8 +1,8 @@
 export const load = async () => {
 	const environment_variables = import.meta.env;
-	const url = `https://api.spoonacular.com/recipes/complexSearch?number=50&apiKey=${environment_variables.VITE_SPOONACULAR_APIK_KEY}`;
-	const res = await fetch(url);
+	const url = `https://api.spoonacular.com/recipes/complexSearch?number=50&apiKey=${environment_variables.VITE_SPOONACULAR_API_KEY}`;
 	console.log(url);
+	const res = await fetch(url);
 	const { results } = await res.json();
 	return {
 		url,
--- food-recipes/src/routes/recipes/[recipeId=number]/+page.server.js
+++ food-recipes/src/routes/recipes/[recipeId=number]/+page.server.js
@@ -4,7 +4,7 @@ import { error } from '@sveltejs/kit';
 export async function load({ params }) {
 	let recipeId = params.recipeId;
 	const environment_variables = import.meta.env;
-	const url = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${environment_variables.VITE_SPOONACULAR_APIK_KEY}`;
+	const url = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${environment_variables.VITE_SPOONACULAR_API_KEY}`;
 	const result = await fetch(url)
 		.then((res) => res.json())
 		.then((data) => data)
